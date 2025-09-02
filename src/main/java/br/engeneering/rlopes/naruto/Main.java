40369f9a9f023909b21814600049821f4fd1e37a
renanrlopes <rrlopes&@gmail.com>
2025-09-02 10:53:21 -0300

    testig the main

--- src/main/java/br/engeneering/rlopes/naruto/Main.java
+++ src/main/java/br/engeneering/rlopes/naruto/Main.java
@@ -3,12 +3,16 @@ package br.engeneering.rlopes.naruto;
 public class Main {
 
     public static void main(String[] args) {
-        KekkeiGenkai r = new KekkeiGenkai(null);
+        Cla cla = new Cla();
+        cla.setNome("Uchiha");
+
+        KekkeiGenkai r = new KekkeiGenkai(cla);
         r.setNome("Rinnegan");
         Shinobi n = new Shinobi();
         n.setNome("Negato");
         n.setApelido("Pain");
         n.setRenegado(true);
         n.getKekkeigenkkais().add(r);
+        System.out.println(r.getCla().getNome());
     }
 }
